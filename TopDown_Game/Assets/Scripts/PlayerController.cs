a55a1d940798ca3a56da1b05d587abe7c67ed5bd
gncano <gcano4501@gmail.com>
2025-10-08 20:19:50 +0200

    rety

--- TopDown_Game/Assets/Scripts/PlayerController.cs
+++ TopDown_Game/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,41 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float speed = 1f;
+    [SerializeField] private float moveSpeed = 1f;
 
+    private PlayerControls playerControls;
+    private Vector2 movement;
+    private Rigidbody2D rb;
+
+    //Uso Awake para que se ejecute antes que el OnEnable
+    private void Awake()
+    {
+        playerControls = new PlayerControls();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    private void Update()
+    {
+        PlayerInput();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void PlayerInput()
+    {
+        movement = playerControls.Movement.Move.ReadValue<Vector2>();
+    }
+
+    private void Move()
+    {
+        rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
+    }
 }
