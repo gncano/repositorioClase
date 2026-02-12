2270bf1b1839cbafac63641b4d6c3c7b6cc447e6
gncano <gcano4501@gmail.com>
2026-02-12 09:25:11 +0100

    hgkl

--- PMM/CanoGonzalo/app/src/main/java/com/gcs/canogonzalo/ui/libro/LibroAdapter.kt
+++ PMM/CanoGonzalo/app/src/main/java/com/gcs/canogonzalo/ui/libro/LibroAdapter.kt
@@ -67,18 +67,3 @@ class LibroAdapter(
 
     override fun getItemCount(): Int = lista.size
 }
-/*
-en el viewholder
-fun bind(libro: LibroConPrestamo) {
-    val color = if (libro.personaId == null) {
-        R.color.verde_libre
-    } else {
-        R.color.rojo_prestado
-    }
-
-    itemView.setBackgroundColor(
-        ContextCompat.getColor(itemView.context, color)
-    )
-}
-
- */
